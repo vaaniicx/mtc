74c76873c3db470a1a1a60b25920f82ae0e8fd5f
vaaniicx <vanessakausl@gmail.com>
2024-01-01 18:00:33 +0100

    Removed commented-out-code, blank lines

--- src/main/java/at/if22b208/mtc/repository/Repository.java
+++ src/main/java/at/if22b208/mtc/repository/Repository.java
@@ -6,12 +6,7 @@ import java.util.Optional;
 public interface Repository<E, T> {
     String SCHEMA = "public.";
     String GENERATE_UUID_SEQUENCE_STRING = "SELECT uuid_generate_v4()";
-
     List<E> findAll();
-
     Optional<E> findById(T t);
-
     E create(E e);
-//    void update(T o);
-//    void deleteById();
 }
