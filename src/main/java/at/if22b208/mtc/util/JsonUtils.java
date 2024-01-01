f8716e908d4c50f6900a249af0e715320af9f9cd
vaaniicx <vanessakausl@gmail.com>
2024-01-01 19:23:42 +0100

    Added java doc and utility method for JsonUtils.java

--- src/main/java/at/if22b208/mtc/util/JsonUtils.java
+++ src/main/java/at/if22b208/mtc/util/JsonUtils.java
@@ -3,14 +3,49 @@ package at.if22b208.mtc.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+
+/**
+ * Utility class for handling JSON serialization and deserialization using Jackson.
+ */
 public class JsonUtils {
     private static final ObjectMapper mapper = new ObjectMapper();
-
     // TODO: Error handling
+
+    /**
+     * Deserializes a JSON string into an object of the specified type.
+     *
+     * @param <T>   The type of the target object.
+     * @param json  The JSON string to deserialize.
+     * @param type  The class representing the target type.
+     * @return      An object of the specified type.
+     * @throws JsonProcessingException If an error occurs during deserialization.
+     */
     public static <T> T getObjectFromJsonString(String json, Class<T> type) throws JsonProcessingException {
         return mapper.readValue(json, type);
     }
 
+    /**
+     * Deserializes a JSON string into a list of objects of the specified type.
+     *
+     * @param <T>   The type of the elements in the list.
+     * @param json  The JSON string to deserialize.
+     * @param type  The class representing the target type.
+     * @return      A list of objects of the specified type.
+     * @throws JsonProcessingException If an error occurs during deserialization.
+     */
+    public static <T> List<T> getListFromJsonString(String json, Class<T> type) throws JsonProcessingException {
+        return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, type));
+    }
+
+    /**
+     * Serializes an object to a JSON string.
+     *
+     * @param <T>     The type of the object.
+     * @param object  The object to serialize.
+     * @return        A JSON string representing the serialized object.
+     * @throws JsonProcessingException If an error occurs during serialization.
+     */
     public static <T> String getJsonStringFromObject(T object) throws JsonProcessingException {
         return mapper.writeValueAsString(object);
     }
