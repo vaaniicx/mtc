fe78d931f70afed64772ad1ac15564020f28dc9f
vaaniicx <vanessakausl@gmail.com>
2024-01-14 17:26:25 +0100

    New ResponseUtils.java for handling responses

---
+++ src/main/java/at/if22b208/mtc/util/ResponseUtils.java
@@ -0,0 +1,44 @@
+package at.if22b208.mtc.util;
+
+import at.if22b208.mtc.config.MessageConstants;
+import at.if22b208.mtc.server.http.ContentType;
+import at.if22b208.mtc.server.http.HttpStatus;
+import at.if22b208.mtc.server.http.Response;
+
+public class ResponseUtils {
+
+    public static Response conflict(String message) {
+        return new Response(HttpStatus.CONFLICT, ContentType.PLAIN_TEXT, message);
+    }
+
+    public static Response created(String message) {
+        return new Response(HttpStatus.CREATED, ContentType.PLAIN_TEXT, message);
+    }
+
+    public static Response error(String message) {
+        return new Response(HttpStatus.INTERNAL_SERVER_ERROR, ContentType.PLAIN_TEXT, message);
+    }
+
+    public static Response forbidden(String message) {
+        return new Response(HttpStatus.FORBIDDEN, ContentType.PLAIN_TEXT, message);
+    }
+
+    public static Response notFound(String message) {
+        return new Response(HttpStatus.NOT_FOUND, ContentType.PLAIN_TEXT, message);
+    }
+
+    public static Response ok(String message) {
+        return new Response(HttpStatus.OK, ContentType.PLAIN_TEXT, message);
+    }
+
+    public static Response ok(ContentType type, String message) {
+        return new Response(HttpStatus.OK, type, message);
+    }
+    public static Response unauthorized() {
+        return new Response(HttpStatus.UNAUTHORIZED, ContentType.PLAIN_TEXT, MessageConstants.UNAUTHORIZED);
+    }
+
+    public static Response unauthorized(String message) {
+        return new Response(HttpStatus.UNAUTHORIZED, ContentType.PLAIN_TEXT, message);
+    }
+}
