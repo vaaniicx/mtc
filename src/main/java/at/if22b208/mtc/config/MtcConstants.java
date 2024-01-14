f6b4f2af1fafdb9a258a5de66115357566a16194
vaaniicx <vanessakausl@gmail.com>
2024-01-14 19:54:44 +0100

    Added MtcConstants.java

---
+++ src/main/java/at/if22b208/mtc/config/MtcConstants.java
@@ -0,0 +1,11 @@
+package at.if22b208.mtc.config;
+
+import java.math.BigInteger;
+
+public class MtcConstants {
+    private MtcConstants() {
+        // hide constructor
+    }
+
+    public static final BigInteger PACKAGE_COST = BigInteger.valueOf(5L);
+}
