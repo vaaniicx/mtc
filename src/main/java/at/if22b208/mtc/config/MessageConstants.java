4ad352ae8bcaa3de71dd1131197dbd63b77c9eda
vaaniicx <vanessakausl@gmail.com>
2024-01-14 13:43:43 +0100

    Added new constant + changed formatting

--- src/main/java/at/if22b208/mtc/config/MessageConstants.java
+++ src/main/java/at/if22b208/mtc/config/MessageConstants.java
@@ -6,6 +6,9 @@ public class MessageConstants {
         // hide constructor
     }
 
+    // GENERAL
+    public static final String UNAUTHORIZED = "Access token is missing or invalid.";
+
     // USER
     public static final String USER_CREATED = "User successfully created.";
     public static final String USER_ALREADY_REGISTERED = "User with same username already registered.";
@@ -15,16 +18,19 @@ public class MessageConstants {
     public static final String USER_LOGIN_INVALID_CREDENTIALS = "Invalid username/password provided";
     public static final String INSUFFICIENT_PRIVILEGE = "Provided user is not \"admin\".";
     public static final String USER_NO_CARDS = "The request was fine, but the user doesn't have any cards.";
+
     // DECK
     public static final String CONFIGURE_DECK = "The deck has been successfully configured.";
     public static final String CONFIGURE_DECK_FAILURE = "The provided deck did not include the required amount of cards.";
     public static final String CONFIGURE_DECK_CARD_UNAVAILABLE = "At least one of the provided cards does not belong to the user or is not available.";
+
     // PACKAGE
     public static final String PACKAGE_CREATED = "Package and cards successfully created.";
     public static final String PACKAGE_CARD_ALREADY_EXISTS = "At least one card in the packages already exists.";
     public static final String PACKAGE_ACQUIRED = "A package has been successfully bought.";
     public static final String INSUFFICIENT_FUNDS = "Not enough money for buying a card package.";
     public static final String NO_PACKAGE_AVAILABLE = "No card package available for buying.";
+
     // TRADING
     public static final String NO_TRADING_DEALS = "The request was fine, but there are no trading deals available.";
     public static final String TRADING_DEAL_CARD_LOCKED = "The deal contains a card that is not owned by the user or locked in the deck.";
