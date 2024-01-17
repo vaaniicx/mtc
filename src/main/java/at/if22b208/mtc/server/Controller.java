7dc5d868254778d9486e361ab651eabc0ffe19bc
vaaniicx <vanessakausl@gmail.com>
2024-01-17 17:11:55 +0100

    Removed unused thrown exceptions from method signature

--- src/main/java/at/if22b208/mtc/server/Controller.java
+++ src/main/java/at/if22b208/mtc/server/Controller.java
@@ -1,13 +1,9 @@
 package at.if22b208.mtc.server;
 
-import at.if22b208.mtc.exception.HashingException;
 import at.if22b208.mtc.server.http.Request;
 import at.if22b208.mtc.server.http.Response;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
 public interface Controller {
-    Response handleRequest(Request request) throws JsonProcessingException, NoSuchAlgorithmException, InvalidKeySpecException, HashingException;
+    Response handleRequest(Request request) throws JsonProcessingException;
 }
