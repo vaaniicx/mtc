f12f10e5ac09d9cdfabbea8ea01fcda94171cd74
vaaniicx <vanessakausl@gmail.com>
2024-01-18 00:06:17 +0100

    Removed exception from method signature

--- src/main/java/at/if22b208/mtc/util/balance/BalanceOperation.java
+++ src/main/java/at/if22b208/mtc/util/balance/BalanceOperation.java
@@ -1,10 +1,9 @@
 package at.if22b208.mtc.util.balance;
 
-import at.if22b208.mtc.exception.BalanceTransactionException;
 import at.if22b208.mtc.exception.NegativeBalanceException;
 
 import java.math.BigInteger;
 
 public interface BalanceOperation {
-    BigInteger operate(BigInteger a, BigInteger b) throws BalanceTransactionException, NegativeBalanceException;
+    BigInteger operate(BigInteger a, BigInteger b) throws NegativeBalanceException;
 }
