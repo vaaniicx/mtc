da55da8c0ed94af465a052a381f057b4392b9154
vaaniicx <vanessakausl@gmail.com>
2024-01-18 21:21:54 +0100

    Added json mapping for UserDataDto.java

--- src/main/java/at/if22b208/mtc/dto/user/UserDataDto.java
+++ src/main/java/at/if22b208/mtc/dto/user/UserDataDto.java
@@ -1,12 +1,18 @@
 package at.if22b208.mtc.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDataDto {
     private String name;
+    @JsonProperty("bio")
     private String biography;
     private String image;
 }
