df9041030883fb78b09c71947cc45323c0908aa2
vaaniicx <vanessakausl@gmail.com>
2024-01-21 01:13:06 +0100

    Added new mapping method from User to UserStatsDto

--- src/main/java/at/if22b208/mtc/util/mapper/UserMapper.java
+++ src/main/java/at/if22b208/mtc/util/mapper/UserMapper.java
@@ -1,13 +1,18 @@
 package at.if22b208.mtc.util.mapper;
 
 import at.if22b208.mtc.dto.user.UserDataDto;
+import at.if22b208.mtc.dto.user.UserStatsDto;
 import at.if22b208.mtc.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    UserDataDto map(User user);
+    UserDataDto mapToUserDataDto(User user);
+
+    @Mapping(source = "username", target="name")
+    UserStatsDto mapToUserStatsDto(User user);
 }
