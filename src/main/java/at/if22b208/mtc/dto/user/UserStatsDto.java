0646cccd0242ab9c3a6883a2a9bb4867fb02ea82
vaaniicx <vanessakausl@gmail.com>
2024-01-21 01:11:39 +0100

    Changed from class to record

--- src/main/java/at/if22b208/mtc/dto/user/UserStatsDto.java
+++ src/main/java/at/if22b208/mtc/dto/user/UserStatsDto.java
@@ -1,11 +1,4 @@
 package at.if22b208.mtc.dto.user;
 
-import lombok.Getter;
-
-@Getter
-public class UserStatsDto {
-    private String name; // TODO: ?
-    private int elo;
-    private int wins;
-    private int losses;
+public record UserStatsDto(String name, int elo, int wins, int losses) {
 }
