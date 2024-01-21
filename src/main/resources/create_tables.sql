1fe3e7f25e03701c679bdc14969fca97f0f25979
vaaniicx <vanessakausl@gmail.com>
2024-01-21 23:44:29 +0100

    Added insert script for tables

---
+++ src/main/resources/create_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS mtc.public.user
+(
+    uuid      uuid PRIMARY KEY NOT NULL,
+    username  varchar          NOT NULL,
+    password  varchar          NOT NULL,
+    balance   int8 default 0,
+    deck      varchar,
+    name      varchar,
+    biography varchar,
+    image     varchar,
+    elo       int4 default 0,
+    wins      int4 default 0,
+    losses    int4 default 0
+);
+
+CREATE TABLE IF NOT EXISTS mtc.public.card
+(
+    uuid       uuid PRIMARY KEY NOT NULL,
+    name       varchar          NOT NULL,
+    damage     int4 default 0,
+    user_uuid  uuid,
+    deck       bool default false,
+    package_id int4             NOT NULL
+);
