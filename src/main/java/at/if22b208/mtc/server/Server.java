1616f9167c388d51d14805e3b5bde7cfd26e575f
vaaniicx <vanessakausl@gmail.com>
2024-06-09 21:46:38 +0200

    Replace usage of sout by a logger

--- src/main/java/at/if22b208/mtc/server/Server.java
+++ src/main/java/at/if22b208/mtc/server/Server.java
@@ -9,8 +9,10 @@ import java.util.concurrent.Executors;
 import at.if22b208.mtc.server.util.RequestHandler;
 import at.if22b208.mtc.server.util.Router;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @AllArgsConstructor
+@Slf4j
 public class Server {
     private int port;
 
@@ -19,8 +21,8 @@ public class Server {
     public void start() {
         final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        System.out.println("Start http-server...");
-        System.out.println("http-server running at: http://localhost:" + this.port);
+        log.info("Start http-server...");
+        log.info("http-server running at: http://localhost:{}", this.port);
 
         try (ServerSocket serverSocket = new ServerSocket(this.port)) {
             while (true) {
